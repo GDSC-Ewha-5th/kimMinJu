5e7825d30202633e2b24f7d2b87a7bbd84caf02a
밍주 <49716188+Minju-Kimm@users.noreply.github.com>
2023-12-02 23:57:52 +0900

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from "react";
+
+
+const FinderWrapper = `
+    width: 800px;
+    height: 600px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
